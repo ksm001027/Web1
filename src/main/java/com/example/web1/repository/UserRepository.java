bc0864a815e9354a5aba4277fa8864bf266e4290
고승민 <ksm001027@naver.com>
2024-05-19 17:55:15 +0900

    로그인 기능 추가

--- src/main/java/com/example/web1/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.web1.repository;
-
-import com.example.web1.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUserID(String userID);
-}
