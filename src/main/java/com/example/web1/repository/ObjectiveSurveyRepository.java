2d3e68269ba1268c4d64590f71b05b1680886bd0
DESKTOP-Q9OBHCS\Yee <ksm001027>
2024-06-06 16:23:57 +0900

    ㄴ

--- src/main/java/com/example/web1/repository/ObjectiveSurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.web1.repository;
-
-import com.example.web1.model.ObjectiveSurvey;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ObjectiveSurveyRepository extends CrudRepository<ObjectiveSurvey, Long> {
-}
--- src/main/java/com/example/web1/repository/SubjectiveQuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.web1.repository;
-
-import com.example.web1.model.SubjectiveQuiz;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectiveQuizRepository extends CrudRepository<SubjectiveQuiz, Long> {
-}
