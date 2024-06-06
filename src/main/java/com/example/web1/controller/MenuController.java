ced8ec01faadfef743d667fef3dadd87340b3278
DESKTOP-Q9OBHCS\Yee <ksm001027>
2024-06-06 17:54:17 +0900

    실시간챗

--- src/main/java/com/example/web1/controller/MenuController.java
+++ src/main/java/com/example/web1/controller/MenuController.java
@@ -3,16 +3,23 @@ package com.example.web1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Random;
 
 @Controller
 public class MenuController {
 
-  @GetMapping("/generate-random-chat")
-  public String generateRandomChat(Model model) {
+  @GetMapping("/generate-chat-room")
+  public String generateChatRoom(@RequestParam String title, Model model) {
     Random random = new Random();
     int roomId = random.nextInt(999999 - 100000) + 100000;
-    return "redirect:/chat/" + roomId;
+    String redirectUrl = UriComponentsBuilder.fromPath("/chat/{roomId}")
+      .queryParam("title", title)
+      .encode()  // URL 인코딩 추가
+      .buildAndExpand(roomId)
+      .toUriString();
+    return "redirect:" + redirectUrl;
   }
 }
