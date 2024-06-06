eb124223642f165b2b043da8895f1223af2ce543
DESKTOP-Q9OBHCS\Yee <ksm001027>
2024-06-06 17:24:40 +0900

    d

--- src/main/java/com/example/web1/controller/ChatController.java
+++ src/main/java/com/example/web1/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.example.web1.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
@@ -12,5 +13,10 @@ public class ChatController {
   @SendTo("/topic/public")
   public ChatMessage sendMessage(ChatMessage chatMessage) {
     return chatMessage;
+  }
+
+  @GetMapping("/chat")
+  public String chat() {
+    return "chat";
   }
 }
